7118975117711aea73d4e794801485ad947e3635
Данил Рощенко <dan.frais2000@gmail.com>
2023-03-03 04:07:08 +0200

    feat/add CustomLogoutView for logout and add route

--- authorization/views.py
+++ authorization/views.py
@@ -1,6 +1,10 @@
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, LogoutView
 
 
 class CustomLoginView(LoginView):
     template_name = 'authorization/login.html'
     success_url = 'home'
+
+
+class CustomLogoutView(LogoutView):
+    next_page = 'login'
